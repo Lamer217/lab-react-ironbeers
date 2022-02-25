b97bd37220665cd2170e167aa27599baeaf64922
Lamer217 <males.public@gmail.com>
2022-02-25 13:23:14 +0100

    display beer info in beer component using fetch

--- src/components/Beers.jsx
+++ src/components/Beers.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import Header from '../components/Header';
 
 export default function Beers() {
+  const [dataFromApi, setDataFromApi] = useState([]);
+  useEffect(() => {
+    fetch('https://ih-beers-api2.herokuapp.com/beers')
+      .then(res => res.json())
+      .then(data => setDataFromApi(data))
+      .catch(err => console.error(err));
+  }, []);
+  console.log(dataFromApi);
   return (
     <div>
       <Header />
+      {dataFromApi.map(beer => (
+        <div className="beer-card" key={beer._id}>
+          <h2>{beer.name}</h2>
+        </div>
+      ))}
     </div>
   );
 }
